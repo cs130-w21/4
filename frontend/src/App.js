fe468408f3a1ce278c293c72c2e61c76dd077de3
Gabriella Long <gabriella.k.long@gmail.com>
2021-02-04 21:02:39 -0800

    aesthetic fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,12 @@ import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
-import React, {useState} from 'react';
-import Modal from 'react-bootstrap/Modal';
+import React, {useState} from 'react'
+import Modal from 'react-bootstrap/Modal'
+import ModalDialog from 'react-bootstrap/ModalDialog'
+import ModalHeader from 'react-bootstrap/ModalHeader'
+import ModalBody from 'react-bootstrap/ModalBody'
+import ModalTitle from 'react-bootstrap/ModalTitle'
 
 
 
@@ -20,22 +24,24 @@ function App() {
           <span className="Nav-bar-title">Summer's Personal Network</span>
         </div>
         <div className="Contact-format">
-          <ButtonGroup>
-            <Button>+ Add Contact</Button>
-            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </DropdownButton>
-          </ButtonGroup>
+
+
+            {/* <Button>+ Add Contact</Button> */}
+
+          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </DropdownButton>
+
           <>
             <Button variant="primary" onClick={handleShow}>
-              Launch demo modal
+              + Add Contact
             </Button>
 
             <Modal show={show} onHide={handleClose}>
               <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>Add a new contact</Modal.Title>
               </Modal.Header>
               <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
               <Modal.Footer>
