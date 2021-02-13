831b060610055f18c336883ab9f7d97137fb1b21
kiannikzad <kian.nikzad@gmail.com>
2021-02-13 00:23:40 -0800

    cookie support

--- Backend/app.js
+++ Backend/app.js
@@ -7,19 +7,24 @@ const fs = require('fs');
 const path = require('path')
 const db = require('./db');
 const port = process.env.PORT || 4001;
-var cookieSession = require('cookie-session')
+var cookieParser = require('cookie-parser')
+var session = require('express-session');
 
 // middleware mounted on all paths
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false}));
-// app.use(cookieSession({
-//     name: 'networkSessionCookie',
-//     keys: [/* secret keys */],
-  
-//     // Cookie Options
-//     maxAge: 24 * 60 * 60 * 1000 // 24 hours
-// }))
+app.use(cookieParser());
+app.use(session({
+    secret: 'myUniqueKeyRatPotatoDog',
+    resave: false,
+    saveUninitialized: false,
+    name: 'networkTrackerSession',
+    cookie: {
+        //1 day
+        maxAge: 86_400_000,
+    }
+}))
 
 // remove "X-Powered-By: Express" from header
 app.set('x-powered-by', false);
@@ -31,18 +36,37 @@ function all(req, res, next) {
 }
 
 async function login(req, res, next) {
-    //if(newCookie)
-        //storeCookieInDatabase;
     userObject = await db.queryUserObject(req.body.username, req.body.password);
-    networkObject = await db.queryNetworkObject(null, userObject.collection);
-    //console.log(networkObject)
+
+    if(userObject == null) {
+        return res.status(401).end()
+    }
+
+    req.session.loggedIn = true;
+    req.session.db_username = userObject.db_username
+    req.session.db_password = userObject.db_password
+
+    networkObject = await db.queryNetworkObject(req.session.db_username, req.session.db_password, userObject.collection);
     response = {"userObject":userObject, "networkObject":networkObject};
-    res.send(response);
+
+    res.status(200).send(response)
+}
+
+async function logout(req, res, next) {
+    //console.log(req.headers)
+    if(req.session.loggedIn != true) {
+        return res.status(200).end()
+    }
+    else {
+        req.session.destroy()
+        return res.status(200).end()
+    }
 }
 
 ////// REGISTER ROUTES //////
 app.get('/', (req, res) => res.sendFile(path.resolve('../frontend/build/index.html')));
 app.post('/api/login', login);
+app.post('/api/logout', logout);
 //app.post('/api/user', user);
 app.all('*', all);
 
