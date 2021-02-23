5e01d0fd18d5134b63affc56b8bf68361589afa0
matthew-dalton <mattdalton@ucla.edu>
2021-02-23 01:17:42 -0800

    Comment out areas that are not working right with backend

--- frontend/src/private-route.js
+++ frontend/src/private-route.js
@@ -10,40 +10,40 @@ import { useAuth } from './use-auth';
 export default function PrivateRoute({children, ...rest}) {
   const auth = useAuth();
 
-  useEffect(() => {
-
-    // if we already have a user object, continue as normal
-    if (auth.user) {
-      return;
-    }
-
-    // if we don't yet have a user object, check if we are signed in
-    const getCore = async () => {
-      const result = await fetch("http://localhost:4001/api/core", {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .catch(err => {
-
-        // remove authentication and redirect to login page
-        if (err.status === 401) {
-          auth.setUser(null);
-        }
-      });
-
-      console.log(result);
-
-      auth.setUser(result.userObject);
-    };
-
-    getCore();
-  },
-  // empty dependency array that way the effect is only triggered on component rendering
-  []);
+  // useEffect(() => {
+  //
+  //   // if we already have a user object, continue as normal
+  //   if (auth.user) {
+  //     return;
+  //   }
+  //
+  //   // if we don't yet have a user object, check if we are signed in
+  //   const getCore = async () => {
+  //     const result = await fetch("http://localhost:4001/api/core", {
+  //       method: 'POST',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //       }
+  //     })
+  //     .then(response => response.json())
+  //     .catch(err => {
+  //
+  //       // remove authentication and redirect to login page
+  //       if (err.status === 401) {
+  //         auth.setUser(null);
+  //       }
+  //     });
+  //
+  //     console.log(result);
+  //
+  //     auth.setUser(result.userObject);
+  //   };
+  //
+  //   getCore();
+  // },
+  // // empty dependency array that way the effect is only triggered on component rendering
+  // []);
 
   return (
     <Route
