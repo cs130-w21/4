5a21b29490178e599507e8c162fa0404f5eca32a
Saumya <saumyadedhia@Saumyas-MacBook-Pro-6.local>
2021-03-03 14:08:09 -0800

    First step of search bar functioning

--- frontend/src/navigation-bar.js
+++ frontend/src/navigation-bar.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from './use-auth'
+import { useCore } from './use-core.js';
 
 export default function NavBar(props) {
 
   const auth = useAuth();
+  let core = useCore();
+  const contacts = core.coreObject.networkObject.contacts;
+  const [input, setInput] = useState('');
 
   const handleLogout = async () => {
     let response = await auth.logout();
@@ -15,25 +19,40 @@ export default function NavBar(props) {
     }
   };
 
+  const updateInput = async (input) => {
+    const filtered = contacts.filter(contact => {
+      console.log('contact', contact)
+     return contact.first.toLowerCase().includes(input.toLowerCase())
+    })
+    setInput(input);
+    console.log("hah", filtered)
+ }
 
   return (
     <div className="Nav-bar">
       <span className="Nav-bar-title">Summer's Personal Network</span>
-      <span class="search-container">
-        <form action="/action_page.php">
-          <input
-            className="Search-bar"
-            key="random1"
-            // value={keyword}
-            placeholder={" Search.."}
-            // onChange={(e) => setKeyword(e.target.value)}
-          />
-        </form>
-      </span>
+      <SearchBar
+        input={input}       
+        setKeyword={updateInput}
+      />
       <div>
         <button onClick={handleLogout}>Sign Out</button>
       </div>
     </div>
   )
 
+}
+
+const SearchBar = ({input, setKeyword}) => {
+  return (
+    <span class="search-container">
+      <input
+        className="Search-bar"
+        key="random1"
+        value={input}
+        placeholder={" Search.."}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+  </span>
+  );
 }
